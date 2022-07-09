02c23b98994a95638f6fdda7e1bddaa85389d686
Sidio01 <sidio1992@gmail.com>
2022-07-09 17:08:28 +0300

    extended test cases

--- internal/adapters/http/server_test.go
+++ internal/adapters/http/server_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	h "gitlab.com/g6834/team26/task/internal/adapters/http"
+	e "gitlab.com/g6834/team26/task/internal/domain/errors"
 	"gitlab.com/g6834/team26/task/internal/domain/models"
 	"gitlab.com/g6834/team26/task/internal/domain/task"
 	"gitlab.com/g6834/team26/task/pkg/api"
@@ -33,7 +34,7 @@ func TestAuthTestSuite(t *testing.T) {
 	suite.Run(t, &authTestSuite{})
 }
 
-func (s *authTestSuite) SetupSuite() {
+func (s *authTestSuite) SetupTest() {
 	// l := zerolog.Nop()
 	l := logger.New()
 
@@ -52,12 +53,11 @@ func (s *authTestSuite) SetupSuite() {
 	go s.srv.Start()
 }
 
-func (s *authTestSuite) TearDownSuite() {
+func (s *authTestSuite) TearDownTest() {
 	_ = s.srv.Stop(context.Background())
 }
 
 func (s *authTestSuite) TestListHandlerSuccess() {
-
 	s.db.On("List", "test123").Return([]*models.Task{&models.Task{UUID: "66f5b904-3f54-4da4-ba74-6dfdf8d72efb",
 		Name:           "test",
 		Text:           "this is test task",
@@ -85,6 +85,34 @@ func (s *authTestSuite) TestListHandlerSuccess() {
 	response.Body.Close()
 }
 
+func (s *authTestSuite) TestListHandlerForbidden() {
+	s.db.On("List", "test123").Return([]*models.Task{&models.Task{UUID: "66f5b904-3f54-4da4-ba74-6dfdf8d72efb",
+		Name:           "test",
+		Text:           "this is test task",
+		InitiatorLogin: "test123",
+		Status:         "created",
+		Approvals: []*models.Approval{&models.Approval{ApprovalLogin: "test626",
+			N:        2,
+			Sent:     sql.NullBool{Valid: true, Bool: false},
+			Approved: sql.NullBool{Valid: false, Bool: false}}}}}, nil)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: false, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, e.ErrAuthFailed)
+
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("GET", "http://localhost:3000/task/v1/tasks/", bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusForbidden, response.StatusCode)
+	response.Body.Close()
+}
+
 func (s *authTestSuite) TestRunHandlerSuccess() {
 	s.db.On("Run", mock.Anything).Return(nil)
 	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
@@ -102,6 +130,46 @@ func (s *authTestSuite) TestRunHandlerSuccess() {
 	// log.Println(string(data))
 	s.NoError(err)
 	s.Equal(http.StatusOK, response.StatusCode)
+	response.Body.Close()
+}
+
+func (s *authTestSuite) TestRunHandlerBadRequest() {
+	s.db.On("Run", mock.Anything).Return(e.ErrInvalidJsonBody)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
+
+	bodyReq := strings.NewReader("{\"approvalLogins\": {\"test626\": \"\", \"zxcvb\": \"\"},\"initiatorLogin\": \"test123\"}")
+
+	req, err := http.NewRequest("POST", "http://localhost:3000/task/v1/tasks/run", bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusBadRequest, response.StatusCode)
+	response.Body.Close()
+}
+
+func (s *authTestSuite) TestRunHandlerForbidden() {
+	s.db.On("Run", mock.Anything).Return(nil)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: false, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, e.ErrAuthFailed)
+
+	bodyReq := strings.NewReader("{\"approvalLogins\": [\"test626\",\"zxcvb\"],\"initiatorLogin\": \"test123\"}")
+
+	req, err := http.NewRequest("POST", "http://localhost:3000/task/v1/tasks/run", bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusForbidden, response.StatusCode)
 	response.Body.Close()
 }
 
@@ -127,6 +195,50 @@ func (s *authTestSuite) TestApproveHandlerSuccess() {
 	response.Body.Close()
 }
 
+func (s *authTestSuite) TestApproveHandlerForbidden() {
+	s.db.On("Approve", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb", "test626").Return(nil)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: false, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, e.ErrAuthFailed)
+
+	approvalLogin := "test626"
+	uuid := "66f5b904-3f54-4da4-ba74-6dfdf8d72efb"
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://localhost:3000/task/v1/tasks/%s/approve/%s", uuid, approvalLogin), bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusForbidden, response.StatusCode)
+	response.Body.Close()
+}
+
+func (s *authTestSuite) TestApproveHandlerNotFound() {
+	s.db.On("Approve", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb", "test626").Return(e.ErrNotFound)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
+
+	approvalLogin := "test626"
+	uuid := "66f5b904-3f54-4da4-ba74-6dfdf8d72efb"
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://localhost:3000/task/v1/tasks/%s/approve/%s", uuid, approvalLogin), bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusNotFound, response.StatusCode)
+	response.Body.Close()
+}
+
 func (s *authTestSuite) TestDeclineHandlerSuccess() {
 	s.db.On("Decline", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb", "test626").Return(nil)
 	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
@@ -149,6 +261,50 @@ func (s *authTestSuite) TestDeclineHandlerSuccess() {
 	response.Body.Close()
 }
 
+func (s *authTestSuite) TestDeclineHandlerForbidden() {
+	s.db.On("Decline", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb", "test626").Return(nil)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: false, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, e.ErrAuthFailed)
+
+	approvalLogin := "test626"
+	uuid := "66f5b904-3f54-4da4-ba74-6dfdf8d72efb"
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://localhost:3000/task/v1/tasks/%s/decline/%s", uuid, approvalLogin), bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusForbidden, response.StatusCode)
+	response.Body.Close()
+}
+
+func (s *authTestSuite) TestDeclineHandlerNotFound() {
+	s.db.On("Decline", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb", "test626").Return(e.ErrNotFound)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
+
+	approvalLogin := "test626"
+	uuid := "66f5b904-3f54-4da4-ba74-6dfdf8d72efb"
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://localhost:3000/task/v1/tasks/%s/decline/%s", uuid, approvalLogin), bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusNotFound, response.StatusCode)
+	response.Body.Close()
+}
+
 func (s *authTestSuite) TestDeleteHandlerSuccess() {
 	s.db.On("Delete", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb").Return(nil)
 	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
@@ -167,5 +323,47 @@ func (s *authTestSuite) TestDeleteHandlerSuccess() {
 	// log.Println(string(data))
 	s.NoError(err)
 	s.Equal(http.StatusOK, response.StatusCode)
+	response.Body.Close()
+}
+
+func (s *authTestSuite) TestDeleteHandlerForbidden() {
+	s.db.On("Delete", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb").Return(nil)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: false, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, e.ErrAuthFailed)
+
+	uuid := "66f5b904-3f54-4da4-ba74-6dfdf8d72efb"
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://localhost:3000/task/v1/tasks/%s", uuid), bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusForbidden, response.StatusCode)
+	response.Body.Close()
+}
+
+func (s *authTestSuite) TestDeleteHandlerNotFound() {
+	s.db.On("Delete", "test123", "66f5b904-3f54-4da4-ba74-6dfdf8d72efb").Return(e.ErrNotFound)
+	s.g.On("Validate", mock.Anything, mock.Anything).Return(&api.AuthResponse{Result: true, Login: "test123", AccessToken: "AccessToken", RefreshToken: "RefreshToken"}, nil)
+
+	uuid := "66f5b904-3f54-4da4-ba74-6dfdf8d72efb"
+	bodyReq := strings.NewReader("")
+
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://localhost:3000/task/v1/tasks/%s", uuid), bodyReq)
+	s.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	// log.Println(err)
+	// data, err := ioutil.ReadAll(response.Body)
+	// log.Println(string(data))
+	s.NoError(err)
+	s.Equal(http.StatusNotFound, response.StatusCode)
 	response.Body.Close()
 }
